82140ae3998adde71449b23d73fce3aa2746ff26
kimkiyong <fenderst0612@gmail.com>
2021-08-13 17:50:43 +0900

    add: CRUD sample

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # go-dynamo-demo
+
+### create table
+```
+$ aws dynamodb create-table --endpoint-url http://localhost:8000 \
+    --table-name MyFirstTable \
+    --attribute-definitions \
+        AttributeName=MyHashKey,AttributeType=S \
+        AttributeName=MyRangeKey,AttributeType=N \
+    --key-schema \
+        AttributeName=MyHashKey,KeyType=HASH \
+        AttributeName=MyRangeKey,KeyType=RANGE \
+--provisioned-throughput \
+        ReadCapacityUnits=1,WriteCapacityUnits=1
+```
+
+### delete table
+```
+$ aws dynamodb delete-table --endpoint-url http://localhost:8000 \
+    --table-name MyFirstTable
+```
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -14,6 +15,13 @@ import (
 const (
 	TokyoResion = "ap-northeast-1"
 )
+
+// Item is struct for DynamoDB
+type Item struct {
+	MyHashKey  string
+	MyRangeKey int
+	MyText     string
+}
 
 func main() {
 
@@ -44,5 +52,48 @@ func main() {
 	})
 
 	table := db.Table("MyFirstTable")
-	_ = table
+
+	/*
+		CRUD DEMO
+	*/
+
+	// CREATE
+	log.Println("START ~create~")
+	item := Item{
+		MyHashKey:  "MyPartitionKey",
+		MyRangeKey: 1,
+		MyText:     "My First Text",
+	}
+	if err := table.Put(item).Run(); err != nil {
+		log.Printf("Failed to put item[%+v]\n", item)
+	}
+	log.Println("END ~create~")
+
+	// READ
+	log.Println("START ~read~")
+	var readResult Item
+	err = table.Get("MyHashKey", item.MyHashKey).Range("MyRangeKey", dynamo.Equal, item.MyRangeKey).One(&readResult)
+	if err != nil {
+		log.Println("Failed to get item")
+	}
+	fmt.Printf("*** Get item[%+v] ***\n", readResult)
+	log.Println("END ~read~")
+
+	// UPDATE
+	log.Println("START ~update~")
+	var updateResult Item
+	err = table.Update("MyHashKey", item.MyHashKey).Range("MyRangeKey", item.MyRangeKey).Set("MyText", "My Second Text").Value(&updateResult)
+	if err != nil {
+		log.Printf("Failed to update item[%+v]\n", err)
+	}
+	log.Println("END ~update~")
+
+	// DELETE
+	log.Println("START ~delete~")
+	err = table.Delete("MyHashKey", item.MyHashKey).Range("MyRangeKey", item.MyRangeKey).Run()
+	if err != nil {
+		log.Printf("Failed to delete item[%+v\n]", item)
+	}
+	log.Println("END ~delete~")
+
 }
