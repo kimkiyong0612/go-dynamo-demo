d0a5903a24451b609b9f7deae3f3fe860979be7f
kimkiyong <fenderst0612@gmail.com>
2021-08-13 16:36:29 +0900

    add: client

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/go-dynamo-demo
 
 go 1.16
+
+require (
+	github.com/aws/aws-sdk-go v1.40.21
+	github.com/guregu/dynamo v1.10.4
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/guregu/dynamo"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/joho/godotenv"
+)
+
+const (
+	TokyoResion = "ap-northeast-1"
+)
 
 func main() {
-	fmt.Println("hello go-dynamodemo")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// config client
+	dynamoDbRegion := os.Getenv("AWS_REGION")
+	disableSsl := false
+
+	// ※dynamodb-localを利用する場合はEndpintのURLを設定する
+	dynamoDbEndpoint := os.Getenv("DYNAMO_ENDPOINT")
+	if len(dynamoDbEndpoint) != 0 {
+		disableSsl = true
+	}
+
+	// defaltで東京リージョンを指定
+	if len(dynamoDbRegion) == 0 {
+		dynamoDbRegion = TokyoResion
+	}
+
+	db := dynamo.New(session.New(), &aws.Config{
+		Region:     aws.String(dynamoDbRegion),
+		Endpoint:   aws.String(dynamoDbEndpoint),
+		DisableSSL: aws.Bool(disableSsl),
+	})
+
+	table := db.Table("MyFirstTable")
+	_ = table
 }
